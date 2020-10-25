c3ec1204c43f10243f26e03f8df066eb79740517
BitwiseV3 <72891149+BitwiseV3@users.noreply.github.com>
2020-10-25 17:43:52 -0400

    Manager.lua created

---
+++ Manager.lua
@@ -0,0 +1,23 @@
+local Interface = {}
+Interface.Locations = {
+	Memory = {},
+	Workspace = {}
+}
+
+
+
+local function FormatModule(ModuleName)
+
+end
+
+local function GetFile(Url)
+
+end
+
+function Interface:Load(ModuleName)
+
+end
+
+function Interface:Get(ModuleName)
+
+end
