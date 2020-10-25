81fad6162680b738f28c418da37db052e4fb4174
BitwiseV3 <72891149+BitwiseV3@users.noreply.github.com>
2020-10-25 17:18:15 -0400

    Initialize.lua updated

--- Initialize.lua
+++ Initialize.lua
@@ -4,6 +4,10 @@ Initialize.InWorkspace = {}
 
 local ModuleDirectory = "https://raw.githubusercontent.com/BitwiseV3/Specter/main/Modules/"
 
+pcall(function()
+    createdirectory("Specter")
+end)
+
 function FormatModuleUrl(ModuleName)
     return ModuleDirectory..tostring(ModuleName)..".lua"
 end
@@ -20,7 +24,19 @@ function GetFile(Url)
 end
 
 function Initialize:Get(ModuleName)
-    
+    if Initialize.Loaded[ModuleName] then
+        return Initialize.Loaded[ModuleName]
+    elseif Initialize.InWorkspace[ModuleName] then
+        local Success, Module = pcall(function()
+            return loadstring(readfile("Specter/"..ModuleName..".lua"))()
+        end)
+        if Success and Module then
+            Initialize.Loaded[ModuleName] = Module
+            return Module
+        end
+    else
+        return Initialize:Load(ModuleName)
+    end 
 end
 
 function Initialize:Load(ModuleName)
@@ -43,6 +59,29 @@ function Initialize:Load(ModuleName)
         end
     else
         warn("Module '"..tostring(ModuleName).."' already loaded.")
+    end
+end
+
+local VersionJSON = "https://raw.githubusercontent.com/BitwiseV3/Specter/main/Version.json"
+local VersionFile = GetFile(VersionJSON)
+local VersionTable = game:GetService("HttpService"):JSONDecode(VersionFile)
+
+for ModuleName, Version in pairs(VersionTable) do
+    local Success, Content = pcall(function()
+        return readfile("Specter/"..ModuleName..".lua")
+    end)
+    if not Success then
+        writefile("Specter/"..ModuleName..".lua", "--"..Version.."\n"..GetFile(FormatModuleUrl(ModuleName)))
+    else
+        local VSub = Content:find("\n")
+        if VSub and type(VSub) == "number" then
+            local CVersion = Content:sub(1, VSub - 1)
+            if CVersion ~= "--"..Version then
+                print("OUT OF DATE UPDATING")
+                writefile("Specter/"..ModuleName..".lua", "--"..Version.."\n"..GetFile(FormatModuleUrl(ModuleName)))
+            end
+        end
+        Initialize.InWorkspace[ModuleName] = true
     end
 end
 
